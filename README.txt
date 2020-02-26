11bdcfd290a4d94d6d658b0e2f93016e21f95f3a
GrandpaBenjamin <GrandpaBenjamin1946@gmail.com>
2020-02-26 18:56:37 +0000

    First Issue
    Read the readme and description

---
+++ README.txt
@@ -0,0 +1,15 @@
+HOla
+
+this file will show you the instructions needed to use the code
+
+1) copy the folder named 'copy and rename - spam'
+2) rename the folder to 'spam'
+3) make sure that the folder has the item 'just a txt file.txt' inside of it
+4) run the code 
+
+IMPORTANT:
+
+DELETE THE FOLDER NAMED 'SPAM' BEFORE RUNNING (IF FOLDER IS ALLREADY MADE)
+CODE WILL CRASH IF THE INSTRUCTIONS ARE NOT CARRIED OUT FULLY OR CORRECTLY
+
+HAVE FUN
---
+++ run.py
@@ -0,0 +1,27 @@
+import shutil
+import os
+import sys
+from time import sleep
+
+num = 0
+num2 = 0
+text = str(num)
+tct = 'VIRUS'+ text +'.txt'
+txt = 'spam/' + tct
+dirName = 'spam'
+
+def check():
+  try:
+    shutil.copy('y.txt', 'tested for folder')
+    return True
+  except FileNotFoundError:
+    return False
+
+
+while True:
+  num += 1
+  text = str(num)
+  tct = 'spam ' + text
+  txt = 'spam/' + tct
+  shutil.copytree('spam', txt)
+  print(tct)
